a6c828210f14fd85b9833da7a4e8f8d3022d5bad
Poliana Sidere <poli360-pp@hotmail.com>
2022-04-05 22:42:47 -0300

    Condições JS

---
+++ Aula 06/ex009.js
@@ -0,0 +1,11 @@
+var vel = 45
+var velMax = 60
+
+if(vel <= velMax){
+    console.log(`A velocidade do seu carro é ${vel}Km/h`)
+    
+} else {
+    console.log(`A velocidade do seu carro é ${vel}Km/h`)
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança`)
